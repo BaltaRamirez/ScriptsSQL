0ceb768ab4ecaa90d482f9bed75dcaed411cb6dd
Balta Ramírez <balta_cruzazul@hotmail.com>
2016-05-30 19:50:32 -0500

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -3,10 +3,14 @@ select * from Cliente C inner join Orden O on C.idCliente = O.idOrden
 		inner join Orden_Detalle OrdDet on O.idOrden = OrdDet.idOrden_Detalle
 			inner join Especialidad E on OrdDet.idOrden_Detalle = E.idEspecialidad
             and E.Tipo = 'Pollo';
-
+            
+            
+#Muestre todos los clientes que hayan ordenado bebidas alcohólicas
 select * from Cliente C inner join Orden O on C.idCliente = O.idOrden
 		inner join Orden_Detalle OrdDet on O.idOrden = OrdDet.idOrden_Detalle
 			inner join Bebida B on OrdDet.idOrden_Detalle = B.idBebida
             and B.Tipo = 'Cerveza';
 
+
+#Muestre cuántas órdenes se encuentran en el status de capturada
 select count(Estado) from Orden where Estado = 'Capturada';
